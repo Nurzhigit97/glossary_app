7cf8b730dac7b3da4a4191323dd89cc3368d544c
Nurzhigit <shaidievn@gmail.com>
2022-12-23 21:06:30 +0600

    add GridView.builder for test to FavouritePage

--- lib/ui/drawer_pages/favourite_page.dart
+++ lib/ui/drawer_pages/favourite_page.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:glossary_app/cubits/Glossary_cubit.dart';
+import 'package:glossary_app/cubits/glossary_state.dart';
+import 'package:glossary_app/ui/screens/detail.dart';
 import 'package:glossary_app/ui/widgets/glossary_ui.dart';
 
-class FavouritePage extends StatelessWidget {
+class FavouritePage extends StatefulWidget {
   const FavouritePage({Key? key}) : super(key: key);
+
+  @override
+  State<FavouritePage> createState() => _FavouritePageState();
+}
+
+class _FavouritePageState extends State<FavouritePage> {
+  @override
+  void initState() {
+    final resData = context.read<GlossaryCubit>();
+    resData.fetchGlossary('');
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +31,55 @@ class FavouritePage extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
-            GlossaryUi(),
+            BlocBuilder<GlossaryCubit, GlossaryState>(
+              builder: ((context, state) {
+                if (state is ErrorGlossaryState) {
+                  return Center(
+                    child: Text(
+                      '${state.errMsg}',
+                      style: TextStyle(fontSize: 20, color: Colors.red),
+                    ),
+                  );
+                }
+                if (state is LoadedGlossaryState) {
+                  final data = state.glossary;
+                  return Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GridView.builder(
+                        itemCount: data.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                        ),
+                        itemBuilder: (context, index) {
+                          final glossary = data[index];
+                          return InkWell(
+                            onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => DetailScreen(
+                                  e: glossary,
+                                ),
+                              ),
+                            ),
+                            child: Card(
+                              elevation: 4,
+                              margin: const EdgeInsets.symmetric(vertical: 3),
+                              child: ListTile(
+                                title: Text('${glossary.title}'),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  );
+                }
+                return Center(child: LinearProgressIndicator());
+              }),
+            ),
           ],
         ),
       ),
