9c1a3d091a55b627272c8d63e4a82fe45c99e45c
Nurzhigit <106031957+Nurzhigit97@users.noreply.github.com>
2022-12-30 17:32:26 +0600

    return previous 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:glossary_app/app.dart';
 import 'package:glossary_app/ui/intro/intro_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-//! firebase
-  await Firebase.initializeApp();
   //! isLogined
   final prefs = await SharedPreferences.getInstance();
   final showHome = await prefs.getBool('showHome') ?? false;
