9257e5f7cad017eba45d2d3d02dabd46c5d191f3
Nurzhigit <shaidievn@gmail.com>
2022-12-30 15:29:41 +0600

    installed and start firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:glossary_app/app.dart';
 import 'package:glossary_app/ui/intro/intro_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+//! firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  //! isLogined
   final prefs = await SharedPreferences.getInstance();
   final showHome = await prefs.getBool('showHome') ?? false;
   runApp(
