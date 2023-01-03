b8ed3830ffe3936c14864cb9918b53d51b98d3cf
Nurzhigit <106031957+Nurzhigit97@users.noreply.github.com>
2023-01-03 16:11:49 +0600

    refactor: get data from glossaryRepo don't from params

--- lib/ui/screens/detail_screen.dart
+++ lib/ui/screens/detail_screen.dart
@@ -1,57 +1,61 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import 'package:glossary_app/data/models/glossary_model.dart';
 import 'package:glossary_app/data/repositories/glossary_repo.dart';
 
-class DetailScreen extends StatefulWidget {
+class DetailScreen extends StatelessWidget {
+  final int? id;
   DetailScreen({
     Key? key,
-    required this.e,
+    required this.id,
   }) : super(key: key);
-//! got user details
-  final GlossaryModel e;
 
-  @override
-  State<DetailScreen> createState() => _DetailScreenState();
-}
-
-class _DetailScreenState extends State<DetailScreen> {
-  final dio = Dio();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('${widget.e.title}'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              GlossaryRepo(dio).updateFavourite(
-                  id: widget.e.id, isFavourite: !widget.e.isFavourite);
-              Navigator.of(context).pop();
-            },
-            icon: Icon(Icons.star,
-                color: widget.e.isFavourite != true
-                    ? Color.fromARGB(255, 255, 255, 255)
-                    : Color.fromARGB(255, 241, 199, 11)),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Text(
-                '${widget.e.description}',
-                style: TextStyle(
-                  fontSize: 20,
+    return FutureBuilder<GlossaryModel?>(
+      future: GlossaryRepo().getGlossary(id: '$id'),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          GlossaryModel? glossary = snapshot.data;
+
+          if (glossary != null) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('${glossary.title}'),
+                actions: [
+                  IconButton(
+                    onPressed: () {
+                      GlossaryRepo().updateFavourite(
+                          id: glossary.id!, isFavourite: !glossary.isFavourite);
+                      Navigator.of(context).pop();
+                    },
+                    icon: Icon(Icons.star,
+                        color: glossary.isFavourite != true
+                            ? Color.fromARGB(255, 255, 255, 255)
+                            : Color.fromARGB(255, 241, 199, 11)),
+                  ),
+                ],
+              ),
+              body: SingleChildScrollView(
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Text(
+                        '${glossary.description}',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            );
+          }
+        }
+        return Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
