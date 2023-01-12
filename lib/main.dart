f3c6e0675e19906ceb810dcf98b17b57037ad7c8
Nurzhigit <106031957+Nurzhigit97@users.noreply.github.com>
2023-01-12 12:04:40 +0600

    refactor: folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:glossary_app/appPage/app.dart';
+import 'package:glossary_app/ui/appPage/app.dart';
 import 'package:glossary_app/resources/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
